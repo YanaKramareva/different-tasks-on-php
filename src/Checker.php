55ef47fb7d2f0b22a0169410f2858b59f969e43a
YanaKramareva <iana2000@mail.ru>
2022-02-05 16:44:30 +0300

    Added new functions

---
+++ src/Checker.php
@@ -0,0 +1,1 @@
+<?phpnamespace App;    use PHPUnit\Runner\AfterLastTestHook;    use App\Tests\CourseTest;final class Checker implements AfterLastTestHook{    public function executeAfterLastTest(): void    {        new CourseTest();    }}
---
+++ src/Course.php
@@ -0,0 +1,1 @@
+<?phpnamespace App;class Course{    private $name;    public function __construct($name)    {        $this->name = $name;    }    public function getName()    {        return $this->name;    }}
